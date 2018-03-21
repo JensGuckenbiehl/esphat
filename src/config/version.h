d75aca7c56ac69b7209f78ed3003f54c0dd4a115
Jens Guckenbiehl <jens.guckenbiehl@abk-technology.de>
2018-03-21 17:31:26 +0100

    moved examples and renamed app

--- src/config/version.h
+++ src/config/version.h
@@ -1,7 +1,7 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define APP_NAME                "Firmeware"
+#define APP_NAME                "esphat"
 #define APP_VERSION             "1.0"
 #define APP_AUTHOR              "jens@werdorf.com"
 #define APP_WEBSITE             "http://www.werdorf.com"
